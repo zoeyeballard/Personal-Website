c6aebc65d1568aa51a2c624348b9a5715fe7e7cd
Zoey Ballard <69688879+zoeyeballard@users.noreply.github.com>
2024-05-14 23:01:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hi");
+    document.addEventListener("DOMContentLoaded", function() {
+        // Your existing slideshow code
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 3000); // Change image every 3 seconds
+        }
+
+        var timeoutID;
+
+        function loading() {
+          timeoutID = setTimeout(showPage, 3000);
+        }
+
+        function showPage() {
+          document.getElementById("loader").style.display = "none";
+          document.getElementById("myDiv").style.display = "block";
+        }
+
+    });
